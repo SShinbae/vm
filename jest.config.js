417eff9e86da070afd1de7cb3c312b3108df7d53
SShinbae <wanahnaf08@gmail.com>
2025-10-19 21:24:26 +0800

    fix: github actions configuration

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
-  preset: "ts-jest",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/coverage/**",
-    "!**/node_modules/**",
-    "!**/babel.config.js",
-    "!**/jest.setup.js",
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@screens(.*)$': '<rootDir>/src/screens$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^@assets(.*)$': '<rootDir>/assets$1',
+    '\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ]
 };
