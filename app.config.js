123ff6eb694b0ae866fe32befc0c48e8797ecf47
Wan Ahnaf <wanahnaf08@gmail.com>
2025-11-30 18:36:45 +0800

    fix: icons on viewing

--- app.config.js
+++ app.config.js
@@ -17,9 +17,8 @@ module.exports = {
       package: "com.vehiclesmanagement.app",
       adaptiveIcon: {
         backgroundColor: "#E6F4FE",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
+        foregroundImage: "./assets/images/vm_logo_foreground.png",
         backgroundImage: "./assets/images/android-icon-background.png",
-        // monochromeImage: "./assets/images/android-icon-monochrome.png", // Commented out to prevent desaturated icon on themed launchers
       },
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
