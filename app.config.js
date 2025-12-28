e538ed14d18f737504ffd524b383696010f5511f
Wan Ahnaf <wanahnaf08@gmail.com>
2025-12-28 20:19:37 +0800

    fix: fix build error and production apk v3

--- app.config.js
+++ app.config.js
@@ -67,10 +67,11 @@ module.exports = {
       [
         "onesignal-expo-plugin",
         {
-          mode: process.env.EAS_BUILD_PROFILE === "production-apk" ||
-                process.env.EAS_BUILD_PROFILE === "production"
-                  ? "production"
-                  : "development",
+          mode:
+            process.env.EAS_BUILD_PROFILE === "production-apk" ||
+            process.env.EAS_BUILD_PROFILE === "production"
+              ? "production"
+              : "development",
         },
       ],
     ],
